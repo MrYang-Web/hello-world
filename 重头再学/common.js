bd6ecc006177e14ac25d6d07c32220e6766fe150
但盼风雨来 <31466416@qq.com>
2019-10-21 23:01:31 +0800

    重头再来

--- 重头再学/common.js
+++ 重头再学/common.js
@@ -1,4 +1,15 @@
-//1.封装一个1-100的随机整数函数
-function randomInt () {
-	var num = parseInt(Math.random()*100+1);
+//1.封装一个1-num的随机整数函数
+function randomInt (num) {
+	var a = parseInt(Math.random()*num+1);
+	return a;
+}
+
+//2.封装生成验证码函数
+//输入n位就生成n位,执行函数会返回一个n位字符串，这里与案例中有区别。
+function createCode (n) {
+		var myCode = '';
+		for (var i=0;i<n;i++) {
+					myCode += parseInt(Math.random()*10);
+		}
+		return myCode;
 }
